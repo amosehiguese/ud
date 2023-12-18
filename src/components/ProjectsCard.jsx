e3bafc38fa94b91142f5a6f24f7c846db3880938
amosehiguese <amosehiguese@gmail.com>
2023-12-18 07:23:18 +0100

    Fix exposed project image

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -10,7 +10,7 @@ export const ProjectsCard = ({ url, img, github, title, text }) => {
         className='w-full object-cover rounded-t-lg h-64'
       />
       <div className='capitalize p-8'>
-        <h2 className='text-xl tracking-wide font-medium'>{title}</h2>
+        <h2 className='text-xl tracking-wide font-bold'>{title}</h2>
         <p className='mt-4 text-slate-700 leading-loose'>{text}</p>
         <div className='mt-4 flex gap-x-4'>
           {url ? (
